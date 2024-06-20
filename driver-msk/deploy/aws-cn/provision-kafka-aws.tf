1b9a3483a050162439b64d3ef79c60230b69e6ef
Kaiming Wan <kaiming.wan@automq.com>
2024-06-20 23:05:58 +0800

    fix: enable multi az for msk

--- driver-msk/deploy/aws-cn/provision-kafka-aws.tf
+++ driver-msk/deploy/aws-cn/provision-kafka-aws.tf
@@ -71,7 +71,7 @@ resource "aws_key_pair" "auth" {
 locals {
   subnet_ids = [
     aws_subnet.subnet_az1.id,
-#     aws_subnet.subnet_az2.id,
+    aws_subnet.subnet_az2.id,
 #     aws_subnet.subnet_az3.id
   ]
 }
@@ -228,9 +228,8 @@ resource "aws_subnet" "subnet_az1" {
   }
 }
 
-// ensure it is in same zone
-resource "aws_subnet" "subnet_az1_2" {
-  availability_zone       = data.aws_availability_zones.azs.names[0]
+resource "aws_subnet" "subnet_az2" {
+  availability_zone       = data.aws_availability_zones.azs.names[1]
   cidr_block              = "192.168.1.0/24"
   vpc_id                  = aws_vpc.benchmark_vpc.id
   map_public_ip_on_launch = true
@@ -300,7 +299,7 @@ resource "aws_msk_cluster" "mskcluster" {
     instance_type  = "kafka.m5.xlarge"
     client_subnets = [
       aws_subnet.subnet_az1.id,
-      aws_subnet.subnet_az1_2.id,
+      aws_subnet.subnet_az2.id,
 #       aws_subnet.subnet_az3.id,
     ]
     storage_info {

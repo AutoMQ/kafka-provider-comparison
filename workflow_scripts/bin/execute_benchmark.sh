a04391b4fa8de25b4d45e3078e5a5b63c556ede9
Kaiming Wan <kaiming.wan@automq.com>
2024-06-22 01:14:40 +0800

    fix: fix echo

--- workflow_scripts/bin/execute_benchmark.sh
+++ workflow_scripts/bin/execute_benchmark.sh
@@ -37,7 +37,7 @@ $SSH_BASE_CMD $SSH_HOST "sudo rm -rf $BENCHMARK_DIR/workflow_scripts/bin/kafka_2
 $SSH_BASE_CMD $SSH_HOST "sudo rm -f $BENCHMARK_DIR/workflow_scripts/bin/bootstrap-server.txt"
 
 
-$SSH_BASE_CMD $SSH_HOST "cd $BENCHMARK_DIR/workflow_scripts/bin && sudo echo $BOOTSTRAP_SERVER >  bootstrap-server.txt"
+$SSH_BASE_CMD $SSH_HOST "cd $BENCHMARK_DIR/workflow_scripts/bin && sudo echo \"$BOOTSTRAP_SERVER\" >  bootstrap-server.txt"
 
 # Execute the benchmark test
 ## Tips: Pay attention that driver.yaml is under /driver-${STREAMING_PROVIDER}

fe6e97269fc09bd926592b6e0979c3447eaa3619
KaimingWan <kaiming.wan@automq.com>
2024-07-05 15:02:26 +0800

    fix: add detailed workload info
    Signed-off-by: Kami Wan <kamiwan@iMac-Pro.local>

--- workflow_scripts/js/generate_report.js
+++ workflow_scripts/js/generate_report.js
@@ -213,7 +213,7 @@ const markdownReport = `
 
   
   #### Elasticity
-  Take how many seconds to move 30 partitions from one broker to another broker after write 210 GB data
+  Take how many seconds to move 30 partitions from one broker to another broker after write 210 GB data (100 MB/s for 35 minutes)
   [AutoMQ]: ${reassignCostSecondsAutoMQ} seconds
   [Kafka]: ${reassignCostSecondsKafka} seconds
   [Amazon MSK]: ${reassignCostSecondsMSK} seconds
